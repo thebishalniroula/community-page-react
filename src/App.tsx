cd90bc063828b6aa4e7c8ad31f02af5aff3f4a29
thebishalniroula <niroulabishal199@gmail.com>
2025-01-20 23:11:21 +0545

    initialize shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <></>;
+  return (
+    <main>
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-2xl font-bold">Posts</h1>
+        <div className="border"></div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
